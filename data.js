f97364617fa5ae3fa5968d22a62da4921a52b8cf
0xDrazilId <fga-as@mail.ru>
2019-07-22 22:21:10 +0400

    1
    1

---
+++ data.js
@@ -0,0 +1,31 @@
+var weatherList = [
+{
+    date: 1559419200000,
+    temperature: {
+      night: 16,
+      day: 26,
+    },
+    cloudiness: 'Ясно',
+    snow: false,
+    rain: false,
+  },
+  {
+    date: 1559505600000,
+    temperature: {
+      night: 19,
+      day: 29,
+    },
+    cloudiness: 'Облачно',
+    snow: false,
+    rain: true,
+  },
+  {
+    date: 1559592000000,
+    temperature: {
+      night: 12,
+      day: 21,
+    },
+    cloudiness: 'Облачно',
+    snow: false,
+    rain: false,
+  }]
